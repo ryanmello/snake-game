b4ca025a74780a1a961475fb98193eaabba64905
Ryan Mello <74434148+ryanmello@users.noreply.github.com>
2023-01-24 09:26:51 -0800

    update

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -1,2 +1,51 @@
-package PACKAGE_NAME;public class GamePanel {
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.util.*;
+
+public class GamePanel extends JPanel implements ActionListener {
+
+    GamePanel(){
+
+    }
+
+    public void startGame(){
+
+    }
+
+    public void paintComponent(Graphics g){
+
+    }
+
+    public void draw(Graphics g){
+
+    }
+
+    public void move(){
+
+    }
+
+    public void checkApple(){
+
+    }
+
+    public void checkCollisions(){
+
+    }
+
+    public void gameOver(Graphics g){
+
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+
+    public static class MyKeyAdapter extends KeyAdapter {
+        @Override
+        public void keyPressed(KeyEvent e){
+
+        }
+    }
 }
